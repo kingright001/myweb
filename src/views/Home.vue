80bea31eb1c4342e8247047d746f542c6c87da64
xieqi <1903918286@qq.com>
2019-12-16 17:07:20 +0800

    初始化项目

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="这个是一个Vue测试项目"/>
   </div>
 </template>
 
---
+++ src/views/Test.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="test">
+        <img src="../assets/logo.png" alt="logo">
+        <h1>这是测试页面的内容</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    h1{
+        color: brown;
+        margin: 0 auto;
+    }
+</style>
